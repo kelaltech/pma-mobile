3a8e27bf0f2768709c8fa5b4d651aaa0a2d69f95
mike <mikiastilahun@gmail.com>
2021-01-02 16:39:31 +0300

    (fix) fix apollo config

--- apollo.config.js
+++ apollo.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  service: {
-    name: 'pma-backend',
-    url: 'https://pma-backend.kelalapp.com/graphql',
-  },
   client: {
+    service: {
+      name: 'pma-backend',
+      url: 'https://pma-backend.kelalapp.com/graphql',
+    },
     includes: ['src/**/*.graphql'],
   },
 };
