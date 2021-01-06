865061ec14cb80ac0bd1628fc39325282c8d9df6
dagem <dagixmeko@gmail.com>
2021-01-06 04:16:01 +0300

    checkin.list

--- src/screens/check-in/check-in.tsx
+++ src/screens/check-in/check-in.tsx
@@ -6,11 +6,22 @@ import Header from '../_shared/header/header';
 import Geolocation from '@react-native-community/geolocation';
 import checkInStyle from './check-in-style';
 import { colors } from '../../assets/styles/colors';
+import { useCheckInQuery } from '../../../gen/apollo-types';
+import Handle from '../_shared/handle/handle';
+import dayjs from 'dayjs';
+
+const siteEngineerId = '613ba210-651a-469c-a690-ad6ecc76a6d5';
 
 const CheckIn = () => {
+  const { loading, data, error, refetch } = useCheckInQuery({
+    variables: { siteEngineerId },
+  });
+
+  const checkins = data?.checkin.getCheckins;
   return (
     <>
       <Header title="PMA" />
+      <Handle {...{ loading, error, data, refetch }}></Handle>
 
       <View style={checkInStyle.titleContainer}>
         <Text style={checkInStyle.title}>Check-ins</Text>
@@ -38,13 +49,17 @@ const CheckIn = () => {
       </Pressable>
 
       <View style={checkInStyle.oldCheckIn}>
-        <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
-          Wed, Dec 23, 2020
-        </Text>
-        <View style={{ flexDirection: 'row' }}>
-          <Text> TIME: 3:15pm </Text>
-          <Text> LOCATION: 38.146, 4.284 </Text>
-        </View>
+        {checkins?.map((checkin) => (
+          <>
+            <Text style={{ fontSize: 16, fontWeight: 'bold' }}>
+              {dayjs(checkin?.created_at).format('MMM DD, YYYY')}
+            </Text>
+            <View style={{ flexDirection: 'row' }}>
+              <Text>{dayjs(checkin?.created_at).format('hh mm a')}</Text>
+              <Text>{checkin?.location} </Text>
+            </View>
+          </>
+        ))}
       </View>
     </>
   );
