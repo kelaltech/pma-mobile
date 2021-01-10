61066deff9e979131a228b3a23521280296f565c
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2021-01-10 17:01:39 +0300

    Fix font linking

--- src/assets/styles/text-styles.ts
+++ src/assets/styles/text-styles.ts
@@ -1,69 +1,69 @@
 import { TextStyle } from 'react-native';
 
 export const fontFamilyHeading = 'Heebo';
-export const fontFamilyBody = 'Heebo';
+export const fontFamilyBody = 'Heebo-Medium';
 
 export const textStyles = {
   h1: {
     fontFamily: fontFamilyHeading,
     fontStyle: 'normal',
-    fontWeight: 'bold',
+    fontWeight: '700',
     fontSize: 36,
     lineHeight: 36 * 1.5,
   } as TextStyle,
   h2: {
     fontFamily: fontFamilyHeading,
     fontStyle: 'normal',
-    fontWeight: 'bold',
+    fontWeight: '700',
     fontSize: 32,
     lineHeight: 32 * 1.5,
   } as TextStyle,
   h3: {
     fontFamily: fontFamilyHeading,
     fontStyle: 'normal',
-    fontWeight: 'bold',
+    fontWeight: '700',
     fontSize: 28,
     lineHeight: 28 * 1.5,
   } as TextStyle,
   h4: {
     fontFamily: fontFamilyHeading,
     fontStyle: 'normal',
-    fontWeight: 'bold',
+    fontWeight: '700',
     fontSize: 24,
     lineHeight: 24 * 1.5,
   } as TextStyle,
   h5: {
     fontFamily: fontFamilyHeading,
     fontStyle: 'normal',
-    fontWeight: 'bold',
+    fontWeight: '700',
     fontSize: 20,
     lineHeight: 20 * 1.5,
   } as TextStyle,
   h6: {
     fontFamily: fontFamilyHeading,
     fontStyle: 'normal',
-    fontWeight: 'bold',
+    fontWeight: '700',
     fontSize: 16,
     lineHeight: 16 * 1.5,
   } as TextStyle,
   large: {
     fontFamily: fontFamilyBody,
     fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontWeight: '400',
     fontSize: 18,
     lineHeight: 18 * 1.5,
   } as TextStyle,
   medium: {
     fontFamily: fontFamilyBody,
     fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontWeight: '400',
     fontSize: 16,
     lineHeight: 16 * 1.5,
   } as TextStyle,
   small: {
     fontFamily: fontFamilyBody,
     fontStyle: 'normal',
-    fontWeight: 'normal',
+    fontWeight: '400',
     fontSize: 14,
     lineHeight: 14 * 1.5,
   } as TextStyle,
